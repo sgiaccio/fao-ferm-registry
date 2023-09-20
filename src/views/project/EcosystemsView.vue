59015ecca4df37a87f3b47c421b7f61562c303e6
Stefano Giaccio <stefano.giaccio@gmail.com>
2023-09-21 00:01:09 +0200

    Improved ecosystem fetch ux

--- src/views/project/EcosystemsView.vue
+++ src/views/project/EcosystemsView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
 
-import { ListBulletIcon } from '@heroicons/vue/20/solid';
+import { ListBulletIcon, ExclamationTriangleIcon, CheckCircleIcon } from '@heroicons/vue/20/solid';
 
 import ButtonWait from '@/components/ButtonWait.vue';
 
@@ -26,17 +26,24 @@ withDefaults(defineProps<{
 function applyToAll() {
     if (!confirm('Are you sure you want to apply this ecosystem to all areas? Your current selections will be overwritten.')) return;
 
-    const key = Object.keys(store.projectAreas[0])[0];
-    const ecosystems = store.projectAreas[0][key].ecosystems;
+    const ecosystems = getAreaValue(store.projectAreas[0]).ecosystems;
+    if (!ecosystems?.length) {
+        alert('Please select ecosystems for the first area first.');
+        return;
+    }
     store.projectAreas.forEach((area, i) => {
+        const type = getAreaType(area);
         if (i > 0) {
-            area[key].ecosystems = ecosystems;
+            area[type].ecosystems = ecosystems;
         }
     });
 }
 
+function getAreaType(area: any) {
+    return Object.keys(area)[0];
+}
 function getAreaValue(area: any) {
-    return area[Object.keys(area)[0]];
+    return area[getAreaType(area)];
 }
 
 async function getBiomeStats(area: any) {
@@ -67,8 +74,15 @@ async function getBiomeStats(area: any) {
     return ecosystems.filter((e: any) => flattenedIucnEcosystems.includes(e));
 }
 
-const areaEcosystemLoading = reactive<boolean[]>([]);
-const anyAreaEcosystemLoading = computed(() => areaEcosystemLoading.some((l) => l));
+const loadAllAreasBiomesButtonPressed = ref(false);
+type Status = 'idle' | 'loading' | 'success' | 'error';
+const areaBiomesLoadingStatus = reactive<Status[]>(new Array(store.projectAreas.length).fill('idle'));
+const anyAreaBiomesLoading = computed(() => areaBiomesLoadingStatus.some(l => l === 'loading'));
+const anyAreaBiomesError = computed(() => areaBiomesLoadingStatus.some(l => l === 'error'));
+const allAreaBiomesSuccess = computed(() => areaBiomesLoadingStatus.every(l => l === 'success'));
+const anyPolygonArea = computed(() => store.projectAreas.some(area => ['upload', 'draw'].includes(getAreaType(area))));
+const loadingAllAreasBiomes = ref(false);
+
 
 async function getAreaBiomeStats(i: number) {
     const area = store.projectAreas[i];
@@ -76,21 +90,28 @@ async function getAreaBiomeStats(i: number) {
     try {
         // check if there are already ecosystems for that area
         if (!areaValue.ecosystems?.length || confirm('Are you sure you want to overwrite the existing ecosystems?')) {
-            areaEcosystemLoading[i] = true;
+            areaBiomesLoadingStatus[i] = 'loading';
             areaValue.ecosystems = await getBiomeStats(area);
+            areaBiomesLoadingStatus[i] = 'success';
         }
     } catch (e) {
         alert('An error occurred while fetching the ecosystems for this area.');
         console.error(e);
-    } finally {
-        areaEcosystemLoading[i] = false;
+        areaBiomesLoadingStatus[i] = 'error';
     }
 }
 
 async function getAllAreasBiomeStats() {
+    loadAllAreasBiomesButtonPressed.value = true;
+    loadingAllAreasBiomes.value = true;
+
     const areas = store.projectAreas;
 
-    // check if any area already has ecosystems
+    // Initialize areaEcosystemLoadingStatus with 'idle'
+    areaBiomesLoadingStatus.length = areas.length;
+    areaBiomesLoadingStatus.fill('loading');
+
+    // Check if any area already has ecosystems
     const askConfirm = areas.some((area) => {
         const areaValue = getAreaValue(area);
         return areaValue.ecosystems?.length;
@@ -98,33 +119,42 @@ async function getAllAreasBiomeStats() {
 
     if (askConfirm && !confirm('Are you sure you want to overwrite the existing ecosystems?')) return;
 
-    const promises = areas.map((area) => getBiomeStats(area));
-
+    const filteredAreas = areas.filter(area => ['upload', 'draw'].includes(getAreaType(area)));
+    const chunkSize = 5; // Number of areas to process at a time
     const errors: string[] = [];
-    let completedPromises = 0;
 
-    promises.forEach(async (p, i) => {
-        try {
-            areaEcosystemLoading[i] = true;
-            const ecosystems = await p;
-            const areaValue = getAreaValue(areas[i]);
-            areaValue.ecosystems = ecosystems;
-        } catch (e) {
-            errors.push(`An error occurred while fetching the ecosystems for area n. ${i + 1}`);
-            console.error(e);
-        } finally {
-            areaEcosystemLoading[i] = false;
-            completedPromises++;
-            
-            // Check if all promises are completed
-            if (completedPromises === promises.length) {
-                if (errors.length) {
-                    alert(errors.join('\n'));
+    for (let i = 0; i < filteredAreas.length; i += chunkSize) {
+        const chunk = filteredAreas.slice(i, i + chunkSize);
+        const promises = chunk.map((area) => getBiomeStats(area));
+
+        await Promise.all(promises.map((p, j) => {
+            return new Promise(async (resolve) => {
+                const index = i + j;
+
+                try {
+                    areaBiomesLoadingStatus[index] = 'loading';
+                    const ecosystems = await p;
+                    const areaValue = getAreaValue(filteredAreas[index]);
+                    areaValue.ecosystems = ecosystems;
+                    areaBiomesLoadingStatus[index] = 'success';
+                } catch (e) {
+                    areaBiomesLoadingStatus[index] = 'error';
+                    errors.push(`An error occurred while fetching the ecosystems for area n. ${index + 1}`);
+                    console.error(e);
+                } finally {
+                    resolve(null);
                 }
-            }
-        }
-    });
+            });
+        }));
+    }
+
+    // Check if there were any errors
+    if (errors.length) {
+        alert(errors.join('\n'));
+    }
+    loadingAllAreasBiomes.value = false;
 }
+
 </script>
 
 <template>
@@ -145,15 +175,28 @@ async function getAllAreasBiomeStats() {
         </template>
         <template #default>
             <div v-if="store.projectAreas?.length"
-                 class="flex flex-col gap-y-4 pt-6">
-                <button type="button"
-                        :disabled="anyAreaEcosystemLoading"
-                        @click="() => getAllAreasBiomeStats()"
-                        :class="[anyAreaEcosystemLoading ? 'bg-gray-100 text-gray-400' : 'bg-ferm-blue-dark-100 hover:bg-ferm-blue-dark-200 text-gray-900', 'relative inline-flex items-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300']">
-                    <ListBulletIcon class="-ml-0.5 h-5 w-5 text-gray-400"
-                                    aria-hidden="true" />
-                    Get ecosystems for all areas
-                </button>
+                 class="flex flex-col gap-y-4 pt-6 mb-6">
+                <div class="flex-shrink justify-self-end ml-auto">
+                    <button v-if="anyPolygonArea && edit"
+                            type="button"
+                            :disabled="anyAreaBiomesLoading"
+                            @click="() => getAllAreasBiomeStats()"
+                            :class="[anyAreaBiomesLoading ? 'bg-gray-100 text-gray-400' : 'bg-ferm-blue-dark-100 hover:bg-ferm-blue-dark-200 text-gray-900', 'relative inline-flex items-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 flex-shrink']">
+                        <ButtonWait v-if="loadingAllAreasBiomes" />
+                        <ExclamationTriangleIcon v-else-if="anyAreaBiomesError && loadAllAreasBiomesButtonPressed"
+                                                 class="-ml-0.5 h-5 w-5 text-red-600"
+                                                 aria-hidden="true" />
+                        <CheckCircleIcon v-else-if="allAreaBiomesSuccess"
+                                         class="-ml-0.5 h-5 w-5 text-green-600"
+                                         aria-hidden="true" />
+                        <ListBulletIcon v-else
+                                        class="-ml-0.5 h-5 w-5 text-gray-400"
+                                        aria-hidden="true" />
+
+
+                        Get ecosystems in all polygon areas
+                    </button>
+                </div>
                 <div v-for="(area, i) in store.projectAreas"
                      class="border-2 px-3 py-2 rounded-lg border-gray-300 dark:border-gray-500">
                     <div class="flex flex-row my-3">
@@ -162,24 +205,32 @@ async function getAllAreasBiomeStats() {
                                   v-if="area[Object.keys(area)[0]].siteName">: {{ area[Object.keys(area)[0]].siteName
                                   }}</span>
                         </div>
-                        <div v-if="edit">
+                        <template v-if="edit">
+                            <button v-if="['upload', 'draw'].includes(getAreaType(area))"
+                                    type="button"
+                                    :disabled="areaBiomesLoadingStatus[i] === 'loading' || areaBiomesLoadingStatus[i] === 'success'"
+                                    @click="() => getAreaBiomeStats(i)"
+                                    :class="[areaBiomesLoadingStatus[i] === 'loading' ? 'bg-gray-100 text-gray-400' : 'bg-ferm-blue-dark-100 text-gray-900', 'relative inline-flex items-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300', ['idle', 'error'].includes(areaBiomesLoadingStatus[i]) ? 'hover:bg-ferm-blue-dark-200' : '']">
+                                <ListBulletIcon v-if="areaBiomesLoadingStatus[i] === 'idle'"
+                                                class="-ml-0.5 h-5 w-5 text-gray-400"
+                                                aria-hidden="true" />
+                                <ButtonWait v-if="areaBiomesLoadingStatus[i] === 'loading'" />
+                                <ExclamationTriangleIcon v-if="areaBiomesLoadingStatus[i] === 'error'"
+                                                         class="-ml-0.5 h-5 w-5 text-red-600"
+                                                         aria-hidden="true" />
+                                <CheckCircleIcon v-if="areaBiomesLoadingStatus[i] === 'success'"
+                                                 class="-ml-0.5 h-5 w-5 text-green-600"
+                                                 aria-hidden="true" />
+                                Get ecosystems in this area
+                            </button>
                             <button v-if="i === 0 && store.projectAreas.length > 1"
                                     type="button"
-                                    class="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    :disabled="!getAreaValue(area).ecosystems?.length"
+                                    :class="[!getAreaValue(area).ecosystems?.length ? 'bg-gray-100 text-gray-400' : 'bg-ferm-blue-dark-100 hover:bg-ferm-blue-dark-200 text-gray-900', 'relative inline-flex items-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 ml-4']"
                                     @click="applyToAll">
                                 Apply to all
                             </button>
-                            <button type="button"
-                                    :disabled="areaEcosystemLoading[i]"
-                                    @click="() => getAreaBiomeStats(i)"
-                                    :class="[areaEcosystemLoading[i] ? 'bg-gray-100 text-gray-400' : 'bg-ferm-blue-dark-100 hover:bg-ferm-blue-dark-200 text-gray-900', 'relative inline-flex items-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300']">
-                                <ListBulletIcon v-if="!areaEcosystemLoading[i]"
-                                                class="-ml-0.5 h-5 w-5 text-gray-400"
-                                                aria-hidden="true" />
-                                <ButtonWait v-else />
-                                Get ecosystems in this area
-                            </button>
-                        </div>
+                        </template>
                     </div>
                     <RecursiveMenu :edit="edit"
                                    v-model="area[Object.keys(area)[0]].ecosystems"
