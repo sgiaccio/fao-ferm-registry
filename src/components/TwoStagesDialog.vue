1b4e92b3a27319c7bd59ed5f9a18a417091ee98e
Stefano Giaccio <stefano.giaccio@gmail.com>
2024-05-22 09:08:00 +0200

    Improving two stages dialog

--- src/components/TwoStagesDialog.vue
+++ src/components/TwoStagesDialog.vue
@@ -35,15 +35,19 @@ defineExpose({
 
 const succeeded = ref<boolean>();
 
+const waiting = ref(false);
+
 async function confirm() {
     try {
         // will show success dialog if no error is thrown. The success dialog needs to be shown
         // after the confirm dialog is closed, so we will open it in the confirmDialogClosed.
         // This is to avoid issues with the UI (scrolling not working after the second dialog is closed).
+        waiting.value = true;
         await props.confirmCallback();
         succeeded.value = true;
         showConfirmDialog.value = false;
     } catch (e) {
+        waiting.value = false;
         succeeded.value = false;
         showConfirmDialog.value = false;
         console.error(e);
@@ -75,28 +79,34 @@ function cancel() {
 </script>
 
 <template>
-    <ConfirmModal :open="showConfirmDialog"
-                  :title="title"
-                  :ok-button-text="confirmButtonText"
-                  :ok-button-enabled="confirmButtonEnabled"
-                  :cancel-button-text="cancelButtonText"
-                  @confirm="confirm"
-                  @cancel="cancel"
-                  @closed="confirmDialogClosed">
+    <ConfirmModal
+        :open="showConfirmDialog"
+        :title="title"
+        :ok-button-text="confirmButtonText"
+        :ok-button-enabled="confirmButtonEnabled && !waiting"
+        :cancel-button-text="cancelButtonText"
+        @confirm="confirm"
+        @cancel="cancel"
+        @closed="confirmDialogClosed"
+    >
         <slot name="confirm" />
     </ConfirmModal>
-    <AlertModal :open="showSuccessDialog"
-                type="success"
-                button-text="OK"
-                :title="successTitle!"
-                @close="complete">
+    <AlertModal
+        :open="showSuccessDialog"
+        type="success"
+        button-text="OK"
+        :title="successTitle!"
+        @close="complete"
+    >
         <slot name="success" />
     </AlertModal>
-    <AlertModal :open="showErrorDialog"
-                type="error"
-                button-text="OK"
-                :title="errorTitle!"
-                @close="() => { showErrorDialog = false }">
+    <AlertModal
+        :open="showErrorDialog"
+        type="error"
+        button-text="OK"
+        :title="errorTitle!"
+        @close="() => { showErrorDialog = false }"
+    >
         <slot name="error" />
     </AlertModal>
 </template>
