a2517ee3d931714eb2f92472ae0023bf5d211eee
Stefano Giaccio <stefano.giaccio@gmail.com>
2024-07-17 14:42:05 +0200

    Removing swiper

--- src/views/project/NakedResultsView.vue
+++ src/views/project/NakedResultsView.vue
@@ -23,18 +23,20 @@ import { MarkerClusterer } from "@googlemaps/markerclusterer";
 import ResultPanel from './ResultPanel.vue';
 
 import Skeleton from 'primevue/skeleton';
+import Galleria from 'primevue/galleria';
 
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import 'swiper/css';
-import 'swiper/css/navigation';
+// import { Swiper, SwiperSlide } from 'swiper/vue';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
 
-// import './style.css';
+// // import './style.css';
 
-// import required modules
-import { Navigation } from 'swiper/modules';
+// // import required modules
+// import { Navigation } from 'swiper/modules';
 
 
-const modules = [Navigation];
+
+// const modules = [Navigation];
 
 // SDG images
 import sdg1 from '@/assets/SDG/E_WEB_01.png';
@@ -230,412 +232,10 @@ import {
     LegendComponent
 } from 'echarts/components';
 
+import echartConfig from './echarts_config.json'
 
 
-
-echarts.registerTheme('dark',
-    {
-        "color": [
-            "#dd6b66",
-            "#759aa0",
-            "#e69d87",
-            "#8dc1a9",
-            "#ea7e53",
-            "#eedd78",
-            "#73a373",
-            "#73b9bc",
-            "#7289ab",
-            "#91ca8c",
-            "#f49f42"
-        ],
-        "backgroundColor": "rgba(51,51,51,1)",
-        "textStyle": {},
-        "title": {
-            "textStyle": {
-                "color": "#eeeeee"
-            },
-            "subtextStyle": {
-                "color": "#aaaaaa"
-            }
-        },
-        "line": {
-            "itemStyle": {
-                "borderWidth": 1
-            },
-            "lineStyle": {
-                "width": 2
-            },
-            "symbolSize": 4,
-            "symbol": "circle",
-            "smooth": false
-        },
-        "radar": {
-            "itemStyle": {
-                "borderWidth": 1
-            },
-            "lineStyle": {
-                "width": 2
-            },
-            "symbolSize": 4,
-            "symbol": "circle",
-            "smooth": false
-        },
-        "bar": {
-            "itemStyle": {
-                "barBorderWidth": 0,
-                "barBorderColor": "#ccc"
-            }
-        },
-        "pie": {
-            "itemStyle": {
-                "borderWidth": 0,
-                "borderColor": "#ccc"
-            }
-        },
-        "scatter": {
-            "itemStyle": {
-                "borderWidth": 0,
-                "borderColor": "#ccc"
-            }
-        },
-        "boxplot": {
-            "itemStyle": {
-                "borderWidth": 0,
-                "borderColor": "#ccc"
-            }
-        },
-        "parallel": {
-            "itemStyle": {
-                "borderWidth": 0,
-                "borderColor": "#ccc"
-            }
-        },
-        "sankey": {
-            "itemStyle": {
-                "borderWidth": 0,
-                "borderColor": "#ccc"
-            }
-        },
-        "funnel": {
-            "itemStyle": {
-                "borderWidth": 0,
-                "borderColor": "#ccc"
-            }
-        },
-        "gauge": {
-            "itemStyle": {
-                "borderWidth": 0,
-                "borderColor": "#ccc"
-            }
-        },
-        "candlestick": {
-            "itemStyle": {
-                "color": "#fd1050",
-                "color0": "#0cf49b",
-                "borderColor": "#fd1050",
-                "borderColor0": "#0cf49b",
-                "borderWidth": 1
-            }
-        },
-        "graph": {
-            "itemStyle": {
-                "borderWidth": 0,
-                "borderColor": "#ccc"
-            },
-            "lineStyle": {
-                "width": 1,
-                "color": "#aaa"
-            },
-            "symbolSize": 4,
-            "symbol": "circle",
-            "smooth": false,
-            "color": [
-                "#dd6b66",
-                "#759aa0",
-                "#e69d87",
-                "#8dc1a9",
-                "#ea7e53",
-                "#eedd78",
-                "#73a373",
-                "#73b9bc",
-                "#7289ab",
-                "#91ca8c",
-                "#f49f42"
-            ],
-            "label": {
-                "color": "#eee"
-            }
-        },
-        "map": {
-            "itemStyle": {
-                "areaColor": "#eee",
-                "borderColor": "#444",
-                "borderWidth": 0.5
-            },
-            "label": {
-                "color": "#000"
-            },
-            "emphasis": {
-                "itemStyle": {
-                    "areaColor": "rgba(255,215,0,0.8)",
-                    "borderColor": "#444",
-                    "borderWidth": 1
-                },
-                "label": {
-                    "color": "rgb(100,0,0)"
-                }
-            }
-        },
-        "geo": {
-            "itemStyle": {
-                "areaColor": "#eee",
-                "borderColor": "#444",
-                "borderWidth": 0.5
-            },
-            "label": {
-                "color": "#000"
-            },
-            "emphasis": {
-                "itemStyle": {
-                    "areaColor": "rgba(255,215,0,0.8)",
-                    "borderColor": "#444",
-                    "borderWidth": 1
-                },
-                "label": {
-                    "color": "rgb(100,0,0)"
-                }
-            }
-        },
-        "categoryAxis": {
-            "axisLine": {
-                "show": true,
-                "lineStyle": {
-                    "color": "#eeeeee"
-                }
-            },
-            "axisTick": {
-                "show": true,
-                "lineStyle": {
-                    "color": "#eeeeee"
-                }
-            },
-            "axisLabel": {
-                "show": true,
-                "color": "#eeeeee"
-            },
-            "splitLine": {
-                "show": true,
-                "lineStyle": {
-                    "color": [
-                        "#aaaaaa"
-                    ]
-                }
-            },
-            "splitArea": {
-                "show": false,
-                "areaStyle": {
-                    "color": [
-                        "#eeeeee"
-                    ]
-                }
-            }
-        },
-        "valueAxis": {
-            "axisLine": {
-                "show": true,
-                "lineStyle": {
-                    "color": "#eeeeee"
-                }
-            },
-            "axisTick": {
-                "show": true,
-                "lineStyle": {
-                    "color": "#eeeeee"
-                }
-            },
-            "axisLabel": {
-                "show": true,
-                "color": "#eeeeee"
-            },
-            "splitLine": {
-                "show": true,
-                "lineStyle": {
-                    "color": [
-                        "#aaaaaa"
-                    ]
-                }
-            },
-            "splitArea": {
-                "show": false,
-                "areaStyle": {
-                    "color": [
-                        "#eeeeee"
-                    ]
-                }
-            }
-        },
-        "logAxis": {
-            "axisLine": {
-                "show": true,
-                "lineStyle": {
-                    "color": "#eeeeee"
-                }
-            },
-            "axisTick": {
-                "show": true,
-                "lineStyle": {
-                    "color": "#eeeeee"
-                }
-            },
-            "axisLabel": {
-                "show": true,
-                "color": "#eeeeee"
-            },
-            "splitLine": {
-                "show": true,
-                "lineStyle": {
-                    "color": [
-                        "#aaaaaa"
-                    ]
-                }
-            },
-            "splitArea": {
-                "show": false,
-                "areaStyle": {
-                    "color": [
-                        "#eeeeee"
-                    ]
-                }
-            }
-        },
-        "timeAxis": {
-            "axisLine": {
-                "show": true,
-                "lineStyle": {
-                    "color": "#eeeeee"
-                }
-            },
-            "axisTick": {
-                "show": true,
-                "lineStyle": {
-                    "color": "#eeeeee"
-                }
-            },
-            "axisLabel": {
-                "show": true,
-                "color": "#eeeeee"
-            },
-            "splitLine": {
-                "show": true,
-                "lineStyle": {
-                    "color": [
-                        "#aaaaaa"
-                    ]
-                }
-            },
-            "splitArea": {
-                "show": false,
-                "areaStyle": {
-                    "color": [
-                        "#eeeeee"
-                    ]
-                }
-            }
-        },
-        "toolbox": {
-            "iconStyle": {
-                "borderColor": "#999999"
-            },
-            "emphasis": {
-                "iconStyle": {
-                    "borderColor": "#666666"
-                }
-            }
-        },
-        "legend": {
-            "textStyle": {
-                "color": "#eeeeee"
-            }
-        },
-        "tooltip": {
-            "axisPointer": {
-                "lineStyle": {
-                    "color": "#eeeeee",
-                    "width": "1"
-                },
-                "crossStyle": {
-                    "color": "#eeeeee",
-                    "width": "1"
-                }
-            }
-        },
-        "timeline": {
-            "lineStyle": {
-                "color": "#eeeeee",
-                "width": 1
-            },
-            "itemStyle": {
-                "color": "#dd6b66",
-                "borderWidth": 1
-            },
-            "controlStyle": {
-                "color": "#eeeeee",
-                "borderColor": "#eeeeee",
-                "borderWidth": 0.5
-            },
-            "checkpointStyle": {
-                "color": "#e43c59",
-                "borderColor": "#c23531"
-            },
-            "label": {
-                "color": "#eeeeee"
-            },
-            "emphasis": {
-                "itemStyle": {
-                    "color": "#a9334c"
-                },
-                "controlStyle": {
-                    "color": "#eeeeee",
-                    "borderColor": "#eeeeee",
-                    "borderWidth": 0.5
-                },
-                "label": {
-                    "color": "#eeeeee"
-                }
-            }
-        },
-        "visualMap": {
-            "color": [
-                "#bf444c",
-                "#d88273",
-                "#f6efa6"
-            ]
-        },
-        "dataZoom": {
-            "backgroundColor": "rgba(47,69,84,0)",
-            "dataBackgroundColor": "rgba(255,255,255,0.3)",
-            "fillerColor": "rgba(167,183,204,0.4)",
-            "handleColor": "#a7b7cc",
-            "handleSize": "100%",
-            "textStyle": {
-                "color": "#eeeeee"
-            }
-        },
-        "markPoint": {
-            "label": {
-                "color": "#eee"
-            },
-            "emphasis": {
-                "label": {
-                    "color": "#eee"
-                }
-            }
-        }
-    }
-);
-
-
-
+echarts.registerTheme('dark', echartConfig);
 
 
 // Features like Universal Transition and Label Layout
@@ -1121,17 +721,17 @@ console.log(getAllActivities());
                     v-if="store.project"
                     class="space-y-4"
                 >
-                    <div class="relative shadow rounded-lg overflow-hidden aspect-[162/100]">
-                        <div class="absolute bottom-0 px-3 py-2 left-0 right-0 z-50 w-full bg-blend-multiply bg-black/60 text-white">
+                    <div class="relative shadow rounded-lg overflow-hidden _aspect-[162/100]">
+                        <!-- <div class="absolute bottom-0 px-3 py-2 left-0 right-0 z-50 w-full bg-blend-multiply bg-red-900 text-white">
                             <div>
                                 <h1 class="text-2xl">{{ store.project.project.title }}</h1>
                             </div>
                             <div class="mt-1 text-base">
                                 <p>{{ countriesString }}</p>
                             </div>
+                        </div> -->
 
-                        </div>
-                        <swiper
+                        <!-- <swiper
                             v-if="uploadedFiles?.length > 0 && uploadedFiles[0]?.imageUrl"
                             :navigation="true"
                             :modules="modules"
@@ -1148,7 +748,31 @@ console.log(getAllActivities());
                                     class="object-cover aspect-[162/100]"
                                 />
                             </swiper-slide>
-                        </swiper>
+                        </swiper> -->
+                        
+                        <!-- <Galleria
+                            v-if="uploadedFiles?.length > 0 && uploadedFiles[0]?.imageUrl" 
+                            :value="uploadedFiles.map(file => ({ itemImageSrc: file.imageUrl, alt: file.name }))"
+                            :responsiveOptions="responsiveOptions" :numVisible="5" :circular="true" containerStyle="_max-width: 640px"
+                            :showItemNavigators="true" :showThumbnails="true">
+                            <template #item="slotProps">
+                                <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="display: block;" class=" object-cover aspect-video"/>
+                            </template>
+                            <template #thumbnail="slotProps">
+                                <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="width: 100%; display: block" class="aspect-video object-cover"/>
+                            </template>
+                        </Galleria>                         -->
+
+                        <Galleria  v-if="uploadedFiles?.length > 0 && uploadedFiles[0]?.imageUrl" :value="uploadedFiles.map(file => ({ itemImageSrc: file.imageUrl, alt: file.name }))" :responsiveOptions="responsiveOptions" :numVisible="5" :thumbnailsPosition="position" containerStyle="max-width: 640px">
+                            <template #item="slotProps">
+                                <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="width: 100%; display: block" />
+                            </template>
+                            <template #thumbnail="slotProps">
+                                <div class="justify-center">
+                                    <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="width: 90%; display: block;" class="object-cover aspect-video" />
+                                </div>
+                            </template>
+                        </Galleria>
                         <Skeleton
                             v-else
                             width="100%"
@@ -1202,7 +826,7 @@ console.log(getAllActivities());
                     </ResultPanel>
 
                     <ResultPanel title="Activities">
-                        {{ getAllACtivities() }}
+                        {{ getAllActivities() }}
                     </ResultPanel>
 
                     <ResultPanel title="Contributions to SDGs">
