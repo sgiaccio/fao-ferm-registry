da321e2ed032213e14f80368a05424628a71a2f9
Stefano Giaccio <stefano.giaccio@gmail.com>
2024-12-13 00:31:37 +0100

    Using toast in main.ts instead of alerts

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,8 @@ import router from './router';
 
 import './index.css'
 
-import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify';
+import Vue3Toasity, { type ToastContainerOptions, toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
-
-// import PrimeVue from 'primevue/config';
-// import Lara from '@/presets/lara';
 
 (async () => {
     const app = createApp(App);
@@ -20,31 +17,17 @@ import 'vue3-toastify/dist/index.css';
         autoClose: 3000
     } as ToastContainerOptions);
 
-    // const auth = useAuthStore();
-    // await auth.fetchUser();
-
-    // !!! ORDER IS IMPORTANT !!!
-    // !!! LOAD ROUTER AFTER THE AUTH STORE IS LOADED AND USER IS FETCHED !!!
     app.use(router);
-
     await router.isReady();
-    // if (router.currentRoute.value.path === '/login' && auth.authLoaded) {
-    //     await router.push(auth.returnUrl);
-    // }
-
 
     app.config.errorHandler = (error, _vm, _info) => {
         // Handle the error globally
         if (process.env.NODE_ENV === 'development') {
-            alert('An unexpected error occurred, please check the console log.');
+            toast.error('An unexpected error occurred, please check the console for more information.', {
+            });
         }
         console.error(error);
     };
 
-    // app.use(PrimeVue, {
-    //     unstyled: true,
-    //     pt: Lara
-    // });
-    
     app.mount('#app');
 })();
