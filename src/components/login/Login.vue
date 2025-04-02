d515f1ff46d72ce63428392a2bebd2d64049ec5d
Stefano Giaccio <stefano.giaccio@fao.org>
2025-04-02 15:30:23 +0200

    Cleaned up Login.vue and auth.ts

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -8,7 +8,7 @@ import router from '../../router'
 import AlertModal from "@/views/AlertModal.vue";
 
 
-const { signInWithEmail, signInWithGoogle, returnUrl } = useAuthStore();
+const { signInWithEmail, signInWithGoogle } = useAuthStore();
 
 const loginForm = ref({ email: null });
 
@@ -41,126 +41,6 @@ function onCloseLoginError() {
 }
 </script>
 
-<!-- <template>
-    <AlertModal
-        type="success"
-        :onClose="onClose"
-        :open="loginSuccess"
-        title="Please check your inbox"
-        buttonText="Ok"
-    >
-        <p class="text-sm text-gray-500">We've sent you an email with instructions to complete your sign-in process. <br>Kindly check your inbox and click on the link provided.</p>
-    </AlertModal>
-
-    <AlertModal
-        type="info"
-        :onClose="onCloseLoginError"
-        :open="loginError"
-        title="Account does not exist"
-        buttonText="Ok"
-    >
-        <p class="text-sm text-gray-500">Sign in error, please try again</p>
-    </AlertModal>
-
-    <div class="flex min-h-full flex-1">
-        <div class="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-            <div class="mx-auto w-full max-w-sm lg:w-96">
-                <div>
-                    <img
-                        class="h-28 w-full"
-                        src="@/assets/UNDecade_LOGO_MASTER_EN.svg"
-                        alt="UN Decade"
-                    >
-                    <h2 class="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900 text-center">Sign up or login to the registry</h2>
-                </div>
-
-                <div class="mt-10">
-                    <div>
-                        <div class="space-y-6">
-                            <div>
-                                <label
-                                    for="email-address"
-                                    class="block text-sm font-medium leading-6 text-gray-900"
-                                >Email address</label>
-                                <div class="mt-2">
-                                    <input
-                                        id="email-address"
-                                        name="email"
-                                        type="email"
-                                        v-model="loginForm.email"
-                                        autocomplete="email"
-                                        required=""
-                                        class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    />
-                                </div>
-                            </div>
-                            <div>
-                                <button
-                                    @click="authenticateWithEmail"
-                                    type="submit"
-                                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                >Email me a sign-in link</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="mt-10">
-                        <div class="relative">
-                            <div
-                                class="absolute inset-0 flex items-center"
-                                aria-hidden="true"
-                            >
-                                <div class="w-full border-t border-gray-200" />
-                            </div>
-                            <div class="relative flex justify-center text-sm font-medium leading-6">
-                                <span class="bg-white px-6 text-gray-900">Or continue with</span>
-                            </div>
-                        </div>
-
-                        <div class="mt-6 grid grid-cols-1 gap-4">
-                            <button
-                                @click="signInWithGoogle"
-                                class="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
-                            >
-                                <svg
-                                    class="h-5 w-5"
-                                    aria-hidden="true"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z"
-                                        fill="#EA4335"
-                                    />
-                                    <path
-                                        d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z"
-                                        fill="#4285F4"
-                                    />
-                                    <path
-                                        d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z"
-                                        fill="#FBBC05"
-                                    />
-                                    <path
-                                        d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z"
-                                        fill="#34A853"
-                                    />
-                                </svg>
-                                <span class="text-sm font-semibold leading-6">Google</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="relative hidden w-0 flex-1 lg:block">
-            <img
-                class="absolute inset-0 h-full w-full object-cover"
-                src="@/assets/login_image.jpg"
-                alt=""
-            />
-        </div>
-    </div>
-</template> -->
-  
 <template>
     <AlertModal type="success"
                 :onClose="onClose"
