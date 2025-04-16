0d5f0f5d6dfc46d64413f428db4edadcc674f2c0
Stefano Giaccio <stefano.giaccio@fao.org>
2025-04-16 16:52:55 +0200

    Adding analytics

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,33 +1,39 @@
 import { initializeApp, getApps } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import { getAuth, connectAuthEmulator, type Auth } from 'firebase/auth';
 import { initializeFirestore, Firestore, connectFirestoreEmulator } from 'firebase/firestore/lite';
 import { getFunctions, connectFunctionsEmulator, type Functions } from 'firebase/functions';
 import { getStorage, connectStorageEmulator, type FirebaseStorage } from 'firebase/storage';
-
-const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
-};
+;
 
 let auth: Auth, db: Firestore;
 let functions: Functions;
 let functionsEuropeWest3: Functions;
 let storage: FirebaseStorage;
+let analytics: any;
 
 // Initialize Firebase
 if (!getApps().length) {
-    const app = initializeApp(firebaseConfig);
+    const app = initializeApp({
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID
+    });
+
+    // Only use analytics in production
+    if (import.meta.env.MODE === 'production') {
+        analytics = getAnalytics(app);
+    }
     db = initializeFirestore(app, {
         ignoreUndefinedProperties: true
     });
 
     auth = getAuth(app);
     functions = getFunctions(app);
-    functionsEuropeWest3 = getFunctions(app, 'europe-west3');
+    functionsEuropeWest3 = getFunctions(app, 'europe-west3'); // ideally all functions should be in EuropeWest3
     storage = getStorage(app);
 
     if (process.env.NODE_ENV === 'development') {
@@ -39,4 +45,4 @@ if (!getApps().length) {
     }
 }
 
-export { auth, db, functions, functionsEuropeWest3, storage };
+export { auth, db, functions, functionsEuropeWest3, storage, analytics };
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,76 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { useLoadingStore } from '../stores/loading';
 
-import { getI18n, t, setLocale, SUPPORT_LOCALES } from '@/lib/i18n';
+import { getI18n, setLocale, SUPPORT_LOCALES } from '@/lib/i18n';
 
 import { useMenusStore } from '../stores/menus';
+import { logEvent } from 'firebase/analytics';
+
+import { analytics } from '@/firebase';
+
+import { getPublicProject } from '@/firebase/functions';
 
 
 // Initiatives tabs are used both for editing and viewing projects, so define them once here
-// Can't set the route name here because they are used twice.
-const projectTabs = [{
-    path: 'general',
-    name: 'projectInfo',
-    component: () => import('../views/project/RegistrationView.vue')
-}, {
-    path: 'area',
-    name: 'projectAoi',
-    component: () => import('../views/project/AoiView.vue')
-}, {
-    path: 'characteristics',
-    name: 'projectCharacteristics',
-    component: () => import('../views/project/characteristics/CharacteristicsView.vue')
-}, {
-    path: 'activities',
-    name: 'projectActivities',
-    component: () => import('../views/project/ActivitiesView.vue')
-}, {
-    path: 'ecosystems',
-    name: 'projectEcosystems',
-    component: () => import('../views/project/EcosystemsView.vue')
-}, {
-    path: 'indicators',
-    name: 'projectIndicators',
-    component: () => import('../views/project/IndicatorsView.vue')
-}, {
-    path: 'monitoring',
-    name: 'projectMonitoring',
-    component: () => import('../views/project/MonitoringView.vue')
-},
-    // {
-    //     path: 'preview',
-    //     name: 'projectResults',
-    //     component: () => import('../views/project/ResultsView.vue')
-    // }
+// Can't set the route name here because they are used twice. Route name is set in the route definition
+const projectTabs = [
+    { path: 'general', name: 'projectInfo', component: () => import('../views/project/RegistrationView.vue'), meta: { title: 'Initiative Information' } },
+    { path: 'area', name: 'projectAoi', component: () => import('../views/project/AoiView.vue'), meta: { title: 'Initiative Area of Interest' } },
+    { path: 'characteristics', name: 'projectCharacteristics', component: () => import('../views/project/characteristics/CharacteristicsView.vue'), meta: { title: 'Initiative Characteristics' } },
+    { path: 'activities', name: 'projectActivities', component: () => import('../views/project/ActivitiesView.vue'), meta: { title: 'Initiative Activities' } },
+    { path: 'ecosystems', name: 'projectEcosystems', component: () => import('../views/project/EcosystemsView.vue'), meta: { title: 'Initiative Ecosystems' } },
+    { path: 'indicators', name: 'projectIndicators', component: () => import('../views/project/IndicatorsView.vue'), meta: { title: 'Initiative Indicators' } },
+    { path: 'monitoring', name: 'projectMonitoring', component: () => import('../views/project/MonitoringView.vue'), meta: { title: 'Initiative Monitoring' } },
 ];
 
 // Good Practices tabs are used both for editing and viewing projects, so define them once here
-const bestPracticeTabs = [{
-    path: 'objectives',
-    name: 'goodPracticesObjectives',
-    component: () => import('../views/bestpractices/ObjectivesView.vue'),
-    meta: { dataPath: 'objectives' }
-}, {
-    path: 'methodology',
-    name: 'goodPracticesMethodology',
-    component: () => import('../views/bestpractices/MethodologyView.vue'),
-    meta: { dataPath: 'methodology' }
-}, {
-    path: 'key-factors',
-    name: 'goodPracticesKeyFactors',
-    component: () => import('../views/bestpractices/KeyFactorsView.vue'),
-    meta: { dataPath: 'keyFactors' }
-}, {
-    path: 'benefits',
-    name: 'goodPracticesBenefits',
-    component: () => import('../views/bestpractices/BenefitsView.vue'),
-    meta: { dataPath: 'benefits' }
-}, {
-    path: 'additional-resources',
-    name: 'goodPracticesAdditionalResources',
-    component: () => import('../views/bestpractices/AdditionalResourcesView.vue'),
-    meta: { dataPath: 'additionalResources' }
-}];
+const bestPracticeTabs = [
+    { path: 'objectives', name: 'goodPracticesObjectives', component: () => import('../views/bestpractices/ObjectivesView.vue'), meta: { title: 'Good Practice Objectives' } },
+    { path: 'methodology', name: 'goodPracticesMethodology', component: () => import('../views/bestpractices/MethodologyView.vue'), meta: { title: 'Good Practice Methodology' } },
+    { path: 'key-factors', name: 'goodPracticesKeyFactors', component: () => import('../views/bestpractices/KeyFactorsView.vue'), meta: { title: 'Good Practice Key Factors, Constraints and Lessons Learned' } },
+    { path: 'benefits', name: 'goodPracticesBenefits', component: () => import('../views/bestpractices/BenefitsView.vue'), meta: { title: 'Good Practice Benefits and Validation' } },
+    { path: 'additional-resources', name: 'goodPracticesAdditionalResources', component: () => import('../views/bestpractices/AdditionalResourcesView.vue'), meta: { title: 'Good Practice Additional Resources' } },
+];
 
 const localesStr = `${SUPPORT_LOCALES.join('|')}`;
 
@@ -103,12 +63,17 @@ const router = createRouter({
             path: `/:locale(${localesStr})?`,
             name: 'home',
             component: () => import('../views/HomeView.vue'),
-            meta: { public: true, title: 'FERM Homepage' }
+            meta: {
+                public: true,
+                title: 'FERM Homepage'
+            }
         }, {
-            // path: '/:locale?/registration',
             path: `/:locale(${localesStr})?/registration`,
             name: 'registration',
-            component: () => import('../views/UserRegistrationView.vue')
+            component: () => import('../views/UserRegistrationView.vue'),
+            meta: {
+                title: 'User Registration'
+            }
         },
 
         // Authentication and registration
@@ -116,20 +81,28 @@ const router = createRouter({
             path: `/:locale(${localesStr})?/login`,
             name: 'login',
             component: () => import('../views/LoginView.vue'),
-            meta: { public: true }
+            meta: {
+                public: true,
+                title: 'Login'
+            }
         },
         {
             path: `/:locale(${localesStr})?/search`,
             name: 'search',
             redirect: { name: 'searchInitiatives' },
-            meta: { public: true },
+            meta: {
+                public: true,
+            },
             children: [
                 {
                     path: 'initiatives',
                     name: 'searchInitiatives',
                     component: () => import('../views/search/SearchView.vue'),
                     props: { type: 'initiatives' },
-                    meta: { public: true }
+                    meta: {
+                        public: true,
+                        title: 'Search for Initiatives'
+                    }
                 }, {
                     // results
                     path: 'initiatives/:id',
@@ -138,13 +111,20 @@ const router = createRouter({
                         _loadMenus();
                     },
                     component: () => import('../views/search/project/ProjectView.vue'),
-                    meta: { newLayout: true, public: true }
+                    meta: {
+                        newLayout: true,
+                        public: true,
+                        title: 'Initiative Details'
+                    }
                 }, {
                     path: 'good-practices',
                     name: 'searchGoodPractices',
                     component: () => import('../views/search/SearchView.vue'),
                     props: { type: 'goodPractices' },
-                    meta: { public: true }
+                    meta: {
+                        public: true,
+                        title: 'Search for Good Practices'
+                    }
                 }, {
                     path: 'good-practices/:id',
                     name: 'publicGoodPracticePage',
@@ -152,7 +132,10 @@ const router = createRouter({
                         _loadMenus();
                     },
                     component: () => import('../views/search/bestPractice/BestPracticeView.vue'),
-                    meta: { public: true }
+                    meta: {
+                        public: true,
+                        title: 'Good Practice Details'
+                    }
                 }
             ]
         },
@@ -175,51 +158,82 @@ const router = createRouter({
             path: `/:locale(${localesStr})?/admin`,
             name: 'admin',
             component: () => import('../views/admin/AdminView.vue'),
+            // meta: {
+            //     title: 'Admin'
+            // },
             children: [
                 {
                     path: 'users',
                     name: 'users',
-                    component: () => import('../views/admin/UserListView.vue')
+                    component: () => import('../views/admin/UserListView.vue'),
+                    meta: {
+                        title: 'Administration - Users'
+                    }
                 }, {
                     path: 'groups',
                     name: 'groups',
-                    component: () => import('../views/admin/GroupListView.vue')
+                    component: () => import('../views/admin/GroupListView.vue'),
+                    meta: {
+                        title: 'Administration - Institutions'
+                    }
                 }, {
                     path: 'submittedInitiatives',
                     name: 'submittedInitiatives',
-                    component: () => import('../views/admin/SubmittedInitiativesView.vue')
+                    component: () => import('../views/admin/SubmittedInitiativesView.vue'),
+                    meta: {
+                        title: 'Administration - Submitted Initiatives'
+                    }
                 }, {
                     path: 'groupAssignments',
                     name: 'groupAssignments',
-                    component: () => import('../views/admin/GroupAssignmentRequests.vue')
+                    component: () => import('../views/admin/GroupAssignmentRequests.vue'),
+                    meta: {
+                        title: 'Administration - Insitution Assignments requests'
+                    }
                 }, {
                     path: 'newGroups',
                     name: 'newGroups',
-                    component: () => import('../views/admin/NewGroupRequestsView.vue')
+                    component: () => import('../views/admin/NewGroupRequestsView.vue'),
+                    meta: {
+                        title: 'Administration - New Institution requests'
+                    }
                 }, {
                     path: 'appState',
                     name: 'appState',
-                    component: () => import('../views/admin/AppState.vue')
+                    component: () => import('../views/admin/AppState.vue'),
+                    meta: {
+                        title: 'Administration - Application State'
+                    }
                 }, {
                     path: 'qc',
                     name: 'qc',
-                    component: () => import('../views/admin/Qc.vue')
+                    component: () => import('../views/admin/Qc.vue'),
+                    meta: {
+                        title: 'Admininstation - Quality Control'
+                    }
                 }
             ]
         }, {
             path: `/:locale(${localesStr})?/registry`,
             name: 'registry',
             component: () => import('../views/RegistryView.vue'),
+            // meta: {
+            //     title: 'Registry'
+            // },
 
             beforeEnter: async (_to, _from) => {
                 _loadMenus();
             },
             redirect: { name: 'initiatives' },
+
             children: [
                 {
                     path: 'newOrganization',
                     name: 'newOrganization',
-                    component: () => import('../views/InstitutionAssignmentPage.vue')
+                    component: () => import('../views/InstitutionAssignmentPage.vue'),
+                    meta: {
+                        title: 'Create a New Organization'
+                    }
                 },
 
                 // Initiatives
@@ -227,10 +241,16 @@ const router = createRouter({
                     path: 'initiatives',
                     name: 'initiatives',
                     component: () => import('../views/project/ProjectListView.vue'),
+                    meta: {
+                        title: 'Initiatives List'
+                    }
                 }, {
                     path: 'initiatives/:id/collaborators',
                     name: 'projectCollaborators',
                     component: () => import('../views/project/CollaboratorsView.vue'),
+                    meta: {
+                        title: 'Initiative Collaborators'
+                    }
                 }, {
                     path: 'initiatives/:id',
                     component: () => import('../views/project/ProjectView.vue'),
@@ -240,28 +260,38 @@ const router = createRouter({
                             name: 'initiative',
                             component: () => import('../views/project/ProjectViewView.vue'),
                             redirect: to => ({ name: 'projectInfo', params: { id: to.params.id } }),
-                            children: [...projectTabs]
+                            // add 'View' to the title
+                            children: projectTabs.map(tab => ({ ...tab, meta: { ...tab.meta, title: `${tab.meta.title}` } }))
                         }, {
                             path: 'print',
                             name: 'printInitiative',
-                            component: () => import('../views/project/ProjectPrintView.vue')
+                            component: () => import('../views/project/ProjectPrintView.vue'),
+                            meta: {
+                                title: 'Print Initiative'
+                            }
                         }, {
                             path: 'edit',
                             name: 'editInitiative',
                             component: () => import('../views/project/ProjectEditView.vue'),
-                            children: projectTabs.map(tab => ({ ...tab, name: `${tab.name}Edit` }))
+                            children: projectTabs.map(tab => ({ ...tab, name: `${tab.name}Edit`, meta: { ...tab.meta, title: `${tab.meta.title} Edit` } }))
                         },
                     ]
                 }, {
                     path: 'auroraImport/:userKey/:auroraProjectId',
                     name: 'auroraImport',
                     component: () => import('../views/AuroraImport.vue'),
+                    meta: {
+                        title: 'Import Aurora Indicators'
+                    }
                 },
 
                 // Good Practices
                 {
                     path: 'good-practices',
-                    component: () => import('../views/bestpractices/BestPracticeListView.vue')
+                    component: () => import('../views/bestpractices/BestPracticeListView.vue'),
+                    meta: {
+                        title: 'Good Practices'
+                    }
                 }, {
                     path: 'good-practices/:projectId/:id',
                     component: () => import('../views/bestpractices/BestPracticeView.vue'),
@@ -270,18 +300,24 @@ const router = createRouter({
                             path: '',
                             name: 'bestPractice',
                             component: () => import('../views/bestpractices/BestPracticeViewView.vue'),
-                            children: [...bestPracticeTabs]
+                            children: bestPracticeTabs.map(tab => ({ ...tab, meta: { ...tab.meta, title: `${tab.meta.title}` } })),
+                            meta: {
+                                title: 'Good Practice'
+                            }
                         },
                         {
                             path: 'print',
                             name: 'printBestPractice',
-                            component: () => import('../views/bestpractices/BestPracticePrint.vue')
+                            component: () => import('../views/bestpractices/BestPracticePrint.vue'),
+                            meta: {
+                                title: 'Print Good Practice'
+                            }
                         },
                         {
                             path: 'edit',
                             name: 'editBestPractice',
                             component: () => import('../views/bestpractices/BestPracticeEditView.vue'),
-                            children: bestPracticeTabs.map(tab => ({ ...tab, name: `${tab.name}Edit` }))
+                            children: bestPracticeTabs.map(tab => ({ ...tab, name: `${tab.name}Edit`, meta: { ...tab.meta, title: `${tab.meta.title} Edit` } })),
                         },
                     ]
                 }
@@ -290,17 +326,23 @@ const router = createRouter({
             path: `/:locale(${localesStr})?/support`,
             name: 'support',
             component: () => import('../views/SupportView.vue'),
-            meta: { public: true }
+            meta: {
+                public: true,
+                title: 'Support'
+            }
         }, {
             path: '/:pathMatch(.*)',
             component: () => import('../views/NotFoundView.vue'),
-            meta: { public: true }
+            meta: {
+                public: true,
+                title: 'Not Found'
+            }
         }
     ],
 });
 
-function setTitle(title: string) {
-    document.title = title || t('fermRegistry');
+function setTitle(title: string, defaultTitle: string) {
+    document.title = title ?? defaultTitle;
 }
 
 router.beforeEach(async (to) => {
@@ -320,7 +362,15 @@ router.beforeEach(async (to) => {
     }
     await setLocale(paramsLocale);
 
-    setTitle(to.meta?.title as string);
+    // Set document.title from route meta, fallback to default
+    const defaultTitle = 'FERM Registry';
+    // if (to.meta && to.meta.title) {
+    //     document.title = to.meta.title as string;
+    // } else {
+    //     document.title = defaultTitle;
+    // }
+
+    setTitle(to.meta?.title + ' - ' + defaultTitle, defaultTitle);
 
     const loadingStore = useLoadingStore();
 
@@ -379,6 +429,7 @@ router.beforeEach(async (to) => {
     // }
 });
 
+let isFirstNavigation = true;
 router.afterEach((to) => {
     const loadingStore = useLoadingStore();
 
@@ -387,6 +438,23 @@ router.afterEach((to) => {
 
     if (to.name === 'registry' || to.matched.some(record => record.name === 'registry')) {
         loadingStore.setRegistryLoaded(true)
+    }
+
+    // Firebase Analytics page_view tracking
+    if (isFirstNavigation) {
+        // If this is the first navigation, it's automatically tracked by Firebase and we don't need to track it again
+        isFirstNavigation = false;
+        return;
+    }
+
+    // const analytics = getAnalyticsInstance();
+    const pageViewEvent = {
+        page_path: to.fullPath,
+        page_title: document.title,
+        page_location: window.location.href,
+    };
+    if (analytics) {
+        logEvent(analytics, 'page_view', pageViewEvent);
     }
 });
 
